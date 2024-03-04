8c00f5f45dfff09d90fab9c432b75b75ce92e5c9
MK Abdul Azees <115166340+mkabdulazees56@users.noreply.github.com>
2024-03-04 22:32:51 +0500

    connected to firsebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import Header from "./componenets/Header/Header";
 import Hero from "./componenets/Hero/Hero";
 import './App.css';
-
-import { getDatabase, ref, set } from "firebase/database";
-import { app } from "./firebase";
 import Companies from "./componenets/Companies/Companies";
 import Residencies from "./componenets/Residencies/Residencies";
 import Value from "./componenets/Value/Value";
@@ -11,9 +8,22 @@ import Contact from "./componenets/Contact/Contact";
 import GetStart from "./componenets/GetStarted/GetStart";
 import Footer from "./componenets/Footer/Footer";
 
+import { getDatabase, ref, set } from "firebase/database";
+import { app } from "./firebase";
+
+
 
 const db = getDatabase(app);
 
+// const addData = ()=>{
+//   set(ref(db,'admin/abdulazees'),{
+//     id:1,
+//     name: "adminname",
+//     age:'20',
+//   })
+// }
+
+// <button onClick={addData}>add data</button>
 
 function App() {
   return (
